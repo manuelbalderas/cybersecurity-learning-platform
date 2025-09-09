d15428b3aea4c033fb33e88fc636ce153403e200
Manuel Balderas <jose.balderas4970@alumnos.udg.mx>
2025-09-08 21:59:27 -0600

    fix: logic with User and streak on chat

--- app/routes/api/chat_socket.py
+++ app/routes/api/chat_socket.py
@@ -15,14 +15,19 @@ def handle_disconnect():
     print('Client disconnected!')
 
 @socket.on('message')
-def handle_message(message):
+def handle_message(data):
+    message = data.get('message')
+    print(message)
+    user_id = data.get('user_id')
+
     print(f"[Socket] Received message: {message}")
     user_id = str(current_user.id) if current_user.is_authenticated else None
 
-    if not current_user.has_done_streak_today and is_message_educational(message):
-        current_user.update_streak()
+    user = User.query.get(user_id) if user_id else None
+
+    if user and not user.has_done_streak_today and is_message_educational(message):
+        user.update_streak()
         db.session.commit()
-        
         flash("¡Has completado tu racha diaria! 🎉", "success")
         # emit('streak_update', {'has_done_streak_today': current_user.has_done_streak_today}, room=request.sid)
         # print(f"User {current_user.username} has done their streak today! Streak: {current_user.has_done_streak_today}")
